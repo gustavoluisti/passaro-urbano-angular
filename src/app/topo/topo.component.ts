5c1b7368932f75e85e6695c1d6188fcb77b72706
Luis Gustavo Santos <gustavoluisrh@gmail.com>
2018-07-30 13:19:41 -0300

    Atualizando parametro de busca com SwitchMap

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs'
+import { Observable, Subject, of } from 'rxjs'
+import { switchMap, debounceTime, distinctUntilChanged, catchError  } from 'rxjs/Operators'
 
 import { OfertasService } from '../ofertas.service'
 import { Oferta } from '../shared/oferta.model'
@@ -13,21 +14,38 @@ import { Oferta } from '../shared/oferta.model'
 export class TopoComponent implements OnInit {
 
   public ofertas: Observable<Oferta[]>
+  private subjectPesquisa: Subject<string> = new Subject<string>()
 
   constructor(private ofertasService: OfertasService) { }
 
   ngOnInit() {
+    this.ofertas = this.subjectPesquisa //retorno Oferta[]
+      .pipe(debounceTime(1000)) //executa a ação do switchMap após 1 segundo
+      .pipe(distinctUntilChanged())
+      .pipe(switchMap((termo: string) => {
+        console.log('requisição http para api')
+
+        if(termo.trim() === '') {
+          //retornar um observable de array de ofertas vazio
+          return of<Oferta[]>([])
+        }
+
+        return this.ofertasService.pesquisaOfertas(termo)
+      }))
+      .pipe(catchError((err: any) => {
+        console.log(err)
+        return of<Oferta[]>([])
+      }))
+
+      this.ofertas.subscribe((ofertas: Oferta[]) => console.log(ofertas))
   }
 
-  public pesquisa(termoDaPesquisa: string): void {
-    this.ofertas = this.ofertasService.pesquisaOfertas(termoDaPesquisa)
-    console.log(this.ofertas)
 
-    this.ofertas.subscribe(
-      (ofertas: Oferta[]) => console.log(ofertas),
-      (erro: any) => console.log('Erro status: ', erro.status),
-      () => console.log('Fluxo de eventos completo!')
-    )
+
+  public pesquisa(termoDaPesquisa: string): void {
+    console.log('keyup: ', termoDaPesquisa)
+    this.subjectPesquisa.next(termoDaPesquisa)
+  
   }
 
 }
